446ffdf73983799272d178581b9652d8002990ae
Dharmesh Khandelwal <dharmesh.khandelwal@mindtree.com>
2017-04-28 15:53:05 +0530

    changed public path

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-app.use('/', express.static('./www'));
-// app.get("/", function(req, res) {
-//     res.json("aaaaaaaaa").send();
-// });
+app.use('/abc', express.static('./www'));
+app.get("/", function(req, res) {
+    res.end("aaaaaaaaa");
+});
 app.set('port', process.env.PORT || 4200);
 app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
