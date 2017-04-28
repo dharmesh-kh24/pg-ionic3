e4ffa0b2f555313e6c3a662ea08df11997377aaf
Dharmesh Khandelwal <dharmesh.khandelwal@mindtree.com>
2017-04-28 15:35:12 +0530

    serve www folder

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
-var express = require('express'),
-    app = express();
-var http = require('http');
-var server = http.createServer(app);
-app.use(express.static(__dirname + '/www'));
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/www/index.html');
-})
-var port = process.env.PORT || 4200;
-server.listen(port, function(req, res) {
-    console.log('server running at port ' + port);
-})
+var express = require('express');
+var app = express();
+app.use(express.static('www'));
+app.set('port', process.env.PORT || 4200);
+app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
