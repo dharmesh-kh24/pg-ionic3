f45266f03648f5666f4bda92533c772978287101
Dharmesh Khandelwal <dharmesh.khandelwal@mindtree.com>
2017-04-28 15:46:57 +0530

    fixed server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var app = express();
-app.use('/', express.static('./www'));
+//app.use('/', express.static('./www'));
+app.get("/", function(req, res) {
+    res.json("aaaaaaaaa").send();
+});
 app.set('port', process.env.PORT || 4200);
 app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
